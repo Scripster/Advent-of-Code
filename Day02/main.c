19e03fce636aad651a2addfc6d5890a71027e0cb
Kjell De Backer <Kjell@Kjells-MacBook-Air.local>
2015-12-14 08:42:11 +0100

    Finished the second part of the second day

--- Day02/main.c
+++ Day02/main.c
@@ -10,18 +10,23 @@ int main(int argc, char **argv) {
 
 	FILE *fp = fopen(argv[1], "r");
 
-	long totalArea = 0;
+	long totalArea = 0, totalRibbon;
 	while (!feof(fp)) {
 		int l, w, h;
 		fscanf(fp, "%i%*c%i%*c%i", &l, &w, &h);
 
 		int area = ((2 * l * w) + (2 * w * h) + (2 * h * l));
 		area += min(l * w, min(w * h, h * l));
-
 		totalArea += area;
+
+		int ribbon = l * w * h;
+		ribbon += min(l + l + w + w, min(w + w + h + h, h + h + l + l));
+
+		totalRibbon += ribbon;
 	}
 
 	fprintf(stdout, "%li square feet of wrapping paper is needed in total.\n", totalArea);
+	fprintf(stdout, "%li feet of ribbon is needed in total.\n", totalRibbon);
 
 	fclose(fp);
 
