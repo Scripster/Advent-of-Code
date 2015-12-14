d74bd0942e006b6c89c20eb95d9dbd1dfd43cfab
Kjell De Backer <Kjell@Kjells-MacBook-Air.local>
2015-12-14 08:35:47 +0100

    Missed a couple of files

---
+++ Day02/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define	min(a, b)	(((a) < (b)) ? (a) : (b))
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: Day01 <input file>\n");
+		return 1;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+
+	long totalArea = 0;
+	while (!feof(fp)) {
+		int l, w, h;
+		fscanf(fp, "%i%*c%i%*c%i", &l, &w, &h);
+
+		int area = ((2 * l * w) + (2 * w * h) + (2 * h * l));
+		area += min(l * w, min(w * h, h * l));
+
+		totalArea += area;
+	}
+
+	fprintf(stdout, "%li square feet of wrapping paper is needed in total.\n", totalArea);
+
+	fclose(fp);
+
+	return 0;
+}
